7ce582faca4d4d5722d4663fdcec2e03504f9411
aitbali <aitbali@hotmail.com>
2018-07-20 19:29:27 +0200

    creatre lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,13 +1,5 @@
-# == Schema Information
-#
-# Table name: lessons
-#
-#  id          :uuid             not null, primary key
-#  description :text
-#  title       :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
+    class Lesson < ApplicationRecord
+        validates :title, presence: true, length: { maximum: 50 }
+        validates :description, length: { maximum: 300 }
+    end
 
-class Lesson < ApplicationRecord
-end
--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -1,17 +1,6 @@
-# == Schema Information
-#
-# Table name: lessons
-#
-#  id          :uuid             not null, primary key
-#  description :text
-#  title       :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
-
 FactoryBot.define do
   factory :lesson do
-    title "MyString"
-    description "MyText"
+    title { Faker::GameOfThrones.character.first(50) }
+    description { Faker::GameOfThrones.quote.first(300) }
   end
 end
