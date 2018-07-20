f34183fe0b43d42c864492d20e0b5dce21fcd5d2
aitbali <aitbali@hotmail.com>
2018-07-20 17:12:38 +0200

    add Factories

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module THP2Zero
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+
+    config.generators do |g|
+      g.factory_bot dir: 'spec/factories'
+    end
   end
 end
