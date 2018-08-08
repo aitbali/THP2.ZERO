94f672e89995c4d14a26022af16a3184fe6a477e
aitbali <aitbali@hotmail.com>
2018-08-08 10:30:03 +0200

    change coverage files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -8,9 +7,6 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'faker'
 require 'shoulda-matchers'
-require 'simplecov'
-SimpleCov.start
-SimpleCov.minimum_coverage 90
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -61,6 +57,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.include JsonHelper
 end
 
 Shoulda::Matchers.configure do |config|
