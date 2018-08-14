cf9649d745a6e12c552747c3664a1537e097592d
aitbali <aitbali@hotmail.com>
2018-08-14 21:41:09 +0200

    fix

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -4,7 +4,7 @@
 #
 #  id          :uuid             not null, primary key
 #  description :text
-#  title       :string
+#  title       :string(50)       not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
