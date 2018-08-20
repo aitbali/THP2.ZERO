09a4e36949ce8d17c5873937ac5ecdc70c97391e
aitbali <aitbali@hotmail.com>
2018-08-20 09:29:07 +0200

    needed params to do action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActionController::ParameterMissing, with: :rescue_param_missing
   rescue_from ActiveRecord::RecordInvalid, with: :rescue_bad_params
